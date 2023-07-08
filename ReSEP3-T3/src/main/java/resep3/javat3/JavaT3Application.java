4a1d3376a4c744cb81ec1227cfe599caa3841d07
Luka Udovicic <lukaudovicic10@gmail.com>
2023-07-08 13:06:51 +0200

    initial T3 commit

---
+++ ReSEP3-T3/src/main/java/resep3/javat3/JavaT3Application.java
@@ -0,0 +1,13 @@
+package resep3.javat3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaT3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaT3Application.class, args);
+    }
+
+}
