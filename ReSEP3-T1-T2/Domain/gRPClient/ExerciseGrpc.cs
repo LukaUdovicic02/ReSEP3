066b8a23704a35603604a00ccf5d60b4f5ca3dd7
ion <ioncnnn@gmail.com>
2023-07-27 09:18:01 +0200

    Exercise grpc

---
+++ ReSEP3-T1-T2/Domain/gRPClient/ExerciseGrpc.cs
@@ -0,0 +1,47 @@
+﻿using domain.grpClient;
+using Domain.gRPClient.RepoInterfaces;
+using Grpc.Net.Client;
+using Model;
+
+namespace Domain.gRPClient;
+
+public class ExerciseGrpc:IRepoExercise
+{
+    public async Task<Exercise> CreateExercise(Exercise exerciseGrpc)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:6565");
+        var client = new ExerciseService.ExerciseServiceClient(channel);
+        Console.WriteLine("Connection ees");
+
+        var reply = await client.createExerciseAsync(new ExerciseRequest
+        {
+            ExName = exerciseGrpc.EName,
+            NrOfSets = exerciseGrpc.NrOfSets,
+            NrOfReps = exerciseGrpc.NrOfReps
+        });
+
+        Console.WriteLine("This is ID of the created exercise: " + reply.ExId);
+
+        return new Exercise
+        {
+            EId = reply.ExId,
+            EName = exerciseGrpc.EName,
+            NrOfSets = exerciseGrpc.NrOfSets,
+            NrOfReps = exerciseGrpc.NrOfReps
+        };
+    }
+
+    public async Task DeleteExercise(int id)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:6565");
+        var client = new ExerciseService.ExerciseServiceClient(channel);
+        Console.WriteLine("Connect yes");
+         
+        var replay=await client.deleteExerciseAsync(new ExerciseRequest 
+        {
+            ExId = id
+        });
+
+        Console.WriteLine("Successfully deleted exercise with ID: "+id);
+    }
+}
---
+++ ReSEP3-T1-T2/Domain/gRPClient/RepoInterfaces/IRepoExercise.cs
@@ -0,0 +1,9 @@
+﻿using Model;
+
+namespace Domain.gRPClient.RepoInterfaces;
+
+public interface IRepoExercise
+{
+    Task<Exercise> CreateExercise(Exercise exercise);
+    Task DeleteExercise(int id);
+}
