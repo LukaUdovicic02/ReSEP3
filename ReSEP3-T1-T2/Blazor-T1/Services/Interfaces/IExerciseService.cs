26f201c2d13433341b66fd42ccdd896bd1df072b
Roalson40 <18957457510tcb@gmail.com>
2023-08-02 18:56:08 +0200

    update

--- ReSEP3-T1-T2/Blazor-T1/Services/Interfaces/IExerciseService.cs
+++ ReSEP3-T1-T2/Blazor-T1/Services/Interfaces/IExerciseService.cs
@@ -4,6 +4,7 @@ using Model;
 
 public interface IExerciseService
 {
+    Task DeleteExercise(int id);
     Task<Exercise> GetExerciseById(int id);
     Task<Exercise> CreateExercise(Exercise exercise);
     Task<List<Exercise>> GetAllExercises();
